4dc627ea1c02393f4ae2167251765f60f6a8b2c3
Foad Ebrahimi <fwad40901@gmail.com>
2024-01-22 22:15:59 +0330

    Add End Section

--- src/assets/javascript/app.js
+++ src/assets/javascript/app.js
@@ -36,29 +36,28 @@ document.addEventListener("keydown", function (e) {
   if (e.key === "ArrowRight") NextSlide();
   e.key === "ArrowLeft" && prevSlide();
 
-// hamburgerMenu
-const ulMenu = document.getElementById("ulMenu");
-const hamburgerMenu = document.getElementById("hamburgerMenu");
-const hamburgerClose = document.getElementById("hamburgerClose");
-const navbar = document.getElementById("navbar");
+  // hamburgerMenu
+  const ulMenu = document.getElementById("ulMenu");
+  const hamburgerMenu = document.getElementById("hamburgerMenu");
+  const hamburgerClose = document.getElementById("hamburgerClose");
+  const navbar = document.getElementById("navbar");
 
-hamburgerMenu.addEventListener("click", ulMenuOpened);
-function ulMenuOpened() {
-  ulMenu.classList.toggle("hidden");
-  navbar.classList.toggle("h-60");
-  navbar.classList.toggle("h-screen");
-  hamburgerMenu.classList.add("hidden");
-  hamburgerClose.classList.remove("hidden");
-}
-hamburgerClose.addEventListener("click", ulMenuClosed);
-function ulMenuClosed() {
-  ulMenu.classList.toggle("hidden");
-  navbar.classList.toggle("h-24");
-  navbar.classList.toggle("h-screen");
-  hamburgerClose.classList.add("hidden");
-  hamburgerMenu.classList.remove("hidden");
-}
-
+  hamburgerMenu.addEventListener("click", ulMenuOpened);
+  function ulMenuOpened() {
+    ulMenu.classList.toggle("hidden");
+    navbar.classList.toggle("h-60");
+    navbar.classList.toggle("h-screen");
+    hamburgerMenu.classList.add("hidden");
+    hamburgerClose.classList.remove("hidden");
+  }
+  hamburgerClose.addEventListener("click", ulMenuClosed);
+  function ulMenuClosed() {
+    ulMenu.classList.toggle("hidden");
+    navbar.classList.toggle("h-24");
+    navbar.classList.toggle("h-screen");
+    hamburgerClose.classList.add("hidden");
+    hamburgerMenu.classList.remove("hidden");
+  }
 });
 btnRight.addEventListener("click", NextSlide);
 btnLeft.addEventListener("click", prevSlide);
